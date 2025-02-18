910f77fd504307c993b12851f6b72bd3e015fc29
asiuzzaman <zaman1304006@gmail.com>
2025-02-19 00:15:51 +0600

    Initial commit

---
+++ MVVMArchitechture/MVVMArchitechture/PostsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMArchitechture
+//
+//  Created by Md. Asiuzzaman on 19/2/25.
+//
+
+import UIKit
+
+class PostsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
